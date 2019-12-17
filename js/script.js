f570e408c2d2d9a0a12d8c7daa3f7a6840d0ed65
Tang Siok Hui <huiendy0913@gmail.com>
2019-12-18 05:04:53 +0800

    Show detail.

--- js/script.js
+++ js/script.js
@@ -51,5 +51,40 @@ $('#search-input').on('keyup', function (e) {
 });
 
 $('#movie-list').on('click', '.see-detail', function () {
-    console.log($(this).data('id'));
+    //   console.log($(this).data('id'));
+
+    $.ajax({
+        url: 'http://omdbapi.com',
+        dataType: 'json',
+        type: 'get',
+        data: {
+            'apikey': 'ea42c1cf',
+            'i': $(this).data('id')
+        },
+        success: function (movie) {
+            if (movie.Response === "True") {
+
+                $('.modal-body').html(`
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <img src="`+ movie.Poster + `" class="img-fluid">
+                    </div>
+
+                    <div class="col-md-8">
+                        <ul class="list-group">
+                            <li class="list-group-item"><h3>`+ movie.Title + `</h3></li>
+                            <li class="list-group-item"><Released :`+ movie.Released + `</li>
+                            <li class="list-group-item"><Released :`+ movie.Released + `</li>
+                            <li class="list-group-item"><Genre :`+ movie.Genre + `</li>
+                            <li class="list-group-item"><Director :`+ movie.Director + `</li>
+                            <li class="list-group-item"><Actor :`+ movie.Actor + `</li>
+                          </ul>
+                     </div>
+                 </div>
+             </div>
+        `);
+            }
+        }
+    })
 });
